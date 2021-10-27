9a152cc34998f204b9053f95100f25b62ae38f30
Sentientnapkin <24svargas@athenian.org>
2021-10-27 14:23:59 -0700

    it work

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,36 @@
 public class Dog extends Pet {
     // Instance variable(s)
+    private String breed;
 
 
     // Constructors
+    public Dog(String name,int age,String breed){
+        super(name,age);
+        this.breed=breed;
+    }
+    public Dog(){
+        super("Roxy",0);
+        this.breed = "labradoodle";
+    }
     
 
     // makeNoise() method
-
+    public void makeNoise(){
+        System.out.println("Woof Woof");
+    }
 
     // toString method
-
+    public String toString() {
+        return "Name: " + this.getName() + ", Age: " + this.getAge()+", Breed: "+ this.breed;
+    }
 
     // Getter
-
+    public String getBreed(){
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed){
+        this.breed=breed;
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,10 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog myDog = new Dog();
+        myDog.setBreed("Golden Retriever");
+        myDog.setAge(3);
+        myDog.setName("Luna");
+        System.out.println(myDog);
+        myDog.makeNoise();
     }
 }
